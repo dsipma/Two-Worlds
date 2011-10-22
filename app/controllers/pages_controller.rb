644eee38eedc1d9c4581ab21acf2e60f5e102e12
Daniel Sipma <dsipma@gmail.com>
2011-10-22 10:35:13 -0500

    cleaning up quotes css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,23 @@
 class PagesController < ApplicationController
   def home
     @posts = Post.all
+    @pagetitle = "Home"
   end
 
   def bio
+    @pagetitle = "Bio"
   end
 
   def order
+    @pagetitle = "Order"
   end
 
   def contact
+    @pagetitle = "Contact"
   end
 
   def book
+    @pagetitle = "Book"
   end
 
 end
