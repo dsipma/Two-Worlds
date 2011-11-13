4326d12b031e2559adc3afcdb1f908133723f3e1
Daniel Sipma <dsipma@gmail.com>
2011-11-13 12:53:01 -0600

    setting up comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :entry, :presence  => true
+  validates :title, :presence  => true
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   validates :entry, :presence  => true
   validates :title, :presence  => true
   default_scope :order => "updated_at DESC", :limit => "3"
+  has_many :comments
 end
