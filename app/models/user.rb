3c5b3522d120b5d43ca580c3b9f9892da2e51395
Daniel Sipma <dsipma@gmail.com>
2011-12-16 22:52:16 -0600

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  def :admin_only
+  def admin_only
     unless current_user.admin
-      redirect_to root_path, :notice => "You must be authorized to go there "
+      redirect_to root_path, :notice => "You must be authorized to go there"
     end
   end
 end
